d9b274c7a25799c75c6937fc8a713b69bc14c0a1
Felipe Silva <felipesilva257@gmail.com>
2021-11-29 00:24:25 -0300

    Upload do Projeto

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://appc.coop.br/pt_br</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://appc.coop.br/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://appc.coop.br/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://appc.coop.br/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://appc.coop.br/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://appc.coop.br/pt_br/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
